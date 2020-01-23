c6f5df8f516501aaa06fccc6e58a37dc2ab70364
Federico Kurganoff <fede.kurganoff@gmailcom>
2020-01-22 23:24:42 -0300

    Commit inicial JuvenilesCAC

---
+++ JuvenilesCAC-SPA/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ValueComponent } from './value/value.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'JuvenilesCAC-SPA';
+}
---
+++ JuvenilesCAC.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using JuvenilesCAC.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuvenilesCAC.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
